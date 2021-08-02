1027de861e1b2a3d3eed9e04a90bd2a17ba18273
Jcob Hippo <s910236@mail.hwsh.tc.edu.tw>
2021-08-02 10:51:05 +0800

    second

--- r.py
+++ r.py
@@ -12,6 +12,22 @@ for line in c:
     d = d + len(line)
 print('平均留言長度', d/len(c))
 
+new = []
+for line in c:
+	if len(line) < 100:
+		new.append(line)
+print('一共有', len(new), '筆資料長度小於100')		
+print(new[0])
+print(new[1])
+
+good =[]
+for line in c:
+	if 'good' in line:
+		good.append(line)
+print('一共有', len(good), '筆資料提到good')
+print(good[0])		
+
+
 
             
 
